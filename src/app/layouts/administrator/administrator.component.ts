57f57981d0a9c3b0b969382c4e31fa3fae5cbd9d
alex.ruiz <alex.ruiz@domus.mx>
2022-03-29 03:11:14 -0600

    error page

--- src/app/layouts/administrator/administrator.component.ts
+++ src/app/layouts/administrator/administrator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-administrator',
@@ -7,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdministratorComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private router: Router) { }
+  logout() {
+    localStorage.removeItem('remember');
+    this.router.navigate([`login`]);
+  }
   ngOnInit(): void {
   }
 
